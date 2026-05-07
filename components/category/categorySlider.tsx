82c4d41793acf31932acc70ff9849279c18b67af
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-05-07 11:30:17 +0500

    Slider

--- components/category/categorySlider.tsx
+++ components/category/categorySlider.tsx
@@ -35,9 +35,8 @@ export default function CategorySlider({ categoriesRes }:any) {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
         }
       },
       {
