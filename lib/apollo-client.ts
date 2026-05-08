70841a539009a9d76ed96931d69b5002dab37ffe
Mufaqar <mufaqar@gmail.com>
2026-05-08 18:01:58 +0500

    .com

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "https://api.halepathpackaging.co.uk/graphql",
+    uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "https://api.halepathpackaging.com/graphql",
   }),
   cache: new InMemoryCache(),
 });
