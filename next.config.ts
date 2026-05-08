45062183deab502d872eaab25bd5b0c8e78e4fea
Mufaqar <mufaqar@gmail.com>
2026-05-08 17:58:01 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      "backend.halepathpackaging.co.uk",
+      "api.halepathpackaging.co.uk",
       
     ],
     remotePatterns: [
