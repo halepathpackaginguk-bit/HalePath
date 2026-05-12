9e7c5ba75160454990681a310c642ee6fabaa3aa
Mufaqar <mufaqar@gmail.com>
2026-05-12 21:25:27 +0500

    tawke.to

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/header";
 import Footer from "@/components/footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,27 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        {children} <Footer />
+        {children} 
+         
+   <Script
+          id="tawkto-script"
+          strategy="afterInteractive"
+        >
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),
+              s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6156ead1d326717cb6843ced/1fgtoahb9';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
+        
+        <Footer />
       </body>
     </html>
   );
