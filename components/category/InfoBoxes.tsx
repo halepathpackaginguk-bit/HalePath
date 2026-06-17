f838685cfb589ed324f803ed8839fa3e7e0bb295
Mufaqar <mufaqar@gmail.com>
2026-06-17 12:33:04 +0500

    Category

--- components/category/InfoBoxes.tsx
+++ components/category/InfoBoxes.tsx
@@ -7,69 +7,72 @@ const InfoBoxes = ({ data }: { data: any }) => {
 
   return (
     <div>
-      <section className="my-10">
-        <div className="hale_container md:flex items-center gap-5 md:gap-10 flex-row-reverse">
-          <figure className="md:w-1/2">
-            <Image
-              alt="Why Us Image"
-              src={infoBox?.infoimage?.node?.mediaItemUrl}
-              width={800}
-              height={600}
-              className="rounded-2xl"
-            />
-          </figure>
+      {infoBox.title && (
+        <section className="my-10">
+          <div className="hale_container md:flex items-center gap-5 md:gap-10 flex-row-reverse">
+            <figure className="md:w-1/2">
+              <Image
+                alt="Why Us Image"
+                src={infoBox?.infoimage?.node?.mediaItemUrl}
+                width={800}
+                height={600}
+                className="rounded-2xl"
+              />
+            </figure>
 
-          <div className="flex md:w-1/2 justify-center md:justify-start items-center md:items-start flex-col cat_info_box">
-            <h4 className="text-[#111827] mt-5 md:mt-0 font-bold text-3xl text-center md:text-left mb-4">
-              {infoBox?.title}
-            </h4>
+            <div className="flex md:w-1/2 justify-center md:justify-start items-center md:items-start flex-col cat_info_box">
+              <h4 className="text-[#111827] mt-5 md:mt-0 font-bold text-3xl text-center md:text-left mb-4">
+                {infoBox?.title}
+              </h4>
 
-            <div
-              className="mb-7 text-center md:text-left"
-              dangerouslySetInnerHTML={{ __html: infoBox.description }}
-            />
+              <div
+                className="mb-7 text-center md:text-left"
+                dangerouslySetInnerHTML={{ __html: infoBox.description }}
+              />
 
-            <Link
-              href="/get-quote-now"
-              className="py-[9px] px-[41px] text-white bg-[#1C2E42] rounded-md"
-            >
-              Get Custom Quote
-            </Link>
+              <Link
+                href="/get-quote-now"
+                className="py-[9px] px-[41px] text-white bg-[#1C2E42] rounded-md"
+              >
+                Get Custom Quote
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
+      {infoBox2.title && (
+        <section className="my-10">
+          <div className="hale_container md:flex items-center gap-5 md:gap-10 flex-row ">
+            <figure className="md:w-1/2">
+              <Image
+                alt="Why Us Image"
+                src={infoBox2?.infoimage?.node?.mediaItemUrl}
+                width={800}
+                height={600}
+                className="rounded-2xl"
+              />
+            </figure>
 
-      <section className="my-10">
-        <div className="hale_container md:flex items-center gap-5 md:gap-10 flex-row ">
-          <figure className="md:w-1/2">
-            <Image
-              alt="Why Us Image"
-              src={infoBox2?.infoimage?.node?.mediaItemUrl}
-              width={800}
-              height={600}
-              className="rounded-2xl"
-            />
-          </figure>
+            <div className="flex md:w-1/2 justify-center md:justify-start items-center md:items-start flex-col cat_info_box">
+              <h4 className="text-[#111827] mt-5 md:mt-0 font-bold text-3xl text-center md:text-left mb-4">
+                {infoBox2.title}
+              </h4>
 
-          <div className="flex md:w-1/2 justify-center md:justify-start items-center md:items-start flex-col cat_info_box">
-            <h4 className="text-[#111827] mt-5 md:mt-0 font-bold text-3xl text-center md:text-left mb-4">
-              {infoBox2.title}
-            </h4>
+              <div
+                className="mb-7 text-center md:text-left"
+                dangerouslySetInnerHTML={{ __html: infoBox2.description }}
+              />
 
-            <div
-              className="mb-7 text-center md:text-left"
-              dangerouslySetInnerHTML={{ __html: infoBox2.description }}
-            />
-
-            <Link
-              href="/get-quote-now"
-              className="py-[9px] px-[41px] text-white bg-[#1C2E42] rounded-md"
-            >
-              Get Custom Quote
-            </Link>
+              <Link
+                href="/get-quote-now"
+                className="py-[9px] px-[41px] text-white bg-[#1C2E42] rounded-md"
+              >
+                Get Custom Quote
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </div>
   );
 };
