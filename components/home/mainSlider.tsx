699684fdb5415ef73fbe9bc328bebae55d4d0e82
Mufaqar <mufaqar@gmail.com>
2026-07-07 14:46:05 +0500

    updated

--- components/home/mainSlider.tsx
+++ components/home/mainSlider.tsx
@@ -8,12 +8,15 @@ export default function MainSlider() {
     <section className="Main_slider">
       <div className="main-slider">
         <div className="w-full relative 2xl:h-[705px] md:h-[605px] flex items-center justify-center overflow-hidden">
-          <iframe
-            className="absolute md:top-1/2 top-[28%] left-1/2 sm:w-[120vw] sm:h-[120vh] w-[468vw] h-[220vh] -translate-x-1/2 -translate-y-1/2"
-            src="https://www.youtube.com/embed/t9cQKmu4TGI?autoplay=1&mute=1&controls=0&loop=1&playlist=t9cQKmu4TGI"
-            allow="autoplay; fullscreen"
-            frameBorder="0"
-          ></iframe>
+          <div className="pointer-events-none">
+            <iframe
+              className="absolute md:top-1/2 top-[28%] left-1/2 sm:w-[120vw] sm:h-[120vh] w-[468vw] h-[220vh] -translate-x-1/2 -translate-y-1/2"
+              src="https://www.youtube.com/embed/t9cQKmu4TGI?autoplay=1&mute=1&controls=0&loop=1&playlist=t9cQKmu4TGI&modestbranding=1&rel=0&iv_load_policy=3"
+              allow="autoplay; fullscreen"
+              frameBorder="0"
+            ></iframe>
+          </div>
+          <div style={{ position: 'absolute', inset: 0, zIndex: 1 }}></div>
           <div className="video_overlay">
             <div className="hale_container relative z-10">
               <div className="md:w-1/2 w-full">
