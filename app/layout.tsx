96252a04751f0bb200edce12b4d5fdaf40a097a3
Mufaqar <mufaqar@gmail.com>
2026-09-16 11:54:08 +0500

    updated data

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,14 @@ export default function RootLayout({
           gtag('js', new Date());
           gtag('config', 'AW-11223196191');`}
         </Script>
+     
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NJ65R553');`}
+        </Script>
         <Suspense fallback={null}>
           <Instagram />
         </Suspense>
